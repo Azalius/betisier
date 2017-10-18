e2b4934d5c25b6417c6928acb18e889e1d4fcdbb
Azalius <mateo1998@live.fr>
2017-10-18 12:38:57 +0200

    try to push with atom

--- include/pages/ModifierPersonne.inc.php
+++ include/pages/ModifierPersonne.inc.php
@@ -1,2 +1,26 @@
+<?php
 
-	<h1>Modifier une personne enregistrées</h1>
+	$pdo = new Mypdo();
+	$villeMan = new VilleManager($pdo);
+ ?>
+
+
+	<h1>Liste des villes</h1>
+	<table>
+		<p>
+			Actuellement
+			<?php echo $villeMan->nbVille(); ?>
+			 villes sont enregistrées
+		</p>
+		<tr>
+			<td>Numéro</td>
+			<td>Nom</td>
+		</tr>
+			<?php
+				$allVilles = $villeMan->getAllVille();
+				foreach ($allVilles as $ville){
+					echo '<tr><td>'.$ville->getNum().'</td><td>'.
+					$ville->getNom().'</td></tr>'."\n";
+				}
+			 ?>
+	</table>
