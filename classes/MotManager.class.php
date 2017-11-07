c69a27c2be7ef570bd366acd7ce99c179719b550
Azalius <mateo1998@live.fr>
2017-11-07 17:26:28 +0100

    working on bad woeds for adding quotes

--- classes/MotManager.class.php
+++ classes/MotManager.class.php
@@ -1,6 +1,7 @@
-<?php public class MotManager{
+<?php
+class MotManager{
   private $bd;
-  private $allBadWord
+  private $allBadWord;
   public function __construct($bede){
     $this->db = $bede;
     $this->allBadWord = array();
@@ -8,20 +9,50 @@
 		$requete = $this->db->prepare($sql);
 		$requete->execute();
 		while($citation = $requete -> fetch(PDO::FETCH_OBJ)){
-			$allBadWord[] = ;
+      $this->allBadWord[] = $citation['mot'];
 		}
 		$requete->closeCursor();
   }
   public function isPhraseOk($phrase){
-
+    if (empty($phrase)){
+      return False;
+    }
+    return count($this->getAllBadWordInPhrase($phrase)) == 0;
   }
   public function isMotOk($mot){
-
+    foreach ($this->allBadWord as $badMot){
+      if ($mot = $badMot){
+        $allBadMot[] = $mot;
+      }
+    }
   }
   public function getCorrectedPhrase($phrase){
-
+    $mots = explode(" ", $phrase);
+    $newPhrase = [];
+    foreach ($mots as $mot) {
+      $lowMot = strtolower($mot);
+      if (!$this->isMotOk($lowMot)){
+        $tiret = "";
+        for ($i = 1 ; i<= strlen($mot) ; $i=$i+1){
+          $tiret = $tiret."-";
+        }
+        $newPhrase[] = $tiret;
+      }
+      else{
+        $newPhrase[] = $mot;
+      }
+    }
+  return $allBadMot;
   }
   public function getAllBadWordInPhrase($phrase){
-
+    $allBadMot = [];
+    $phrase = strtolower($phrase);
+    $mots = explode(" ", $phrase);
+    foreach ($mots as $mot) {
+      if (!$this->isMotOk($mot)){
+        $allBadMot[] = $mot;
+      }
+    }
+  return $allBadMot;
   }
 } ?>
--- include/pages/ajouterCitation.inc.php
+++ include/pages/ajouterCitation.inc.php
@@ -2,24 +2,31 @@
 	$pdo = new Mypdo();
 	$persMan = new PersonneManager($pdo);
   $citMan = new CitationsManager($pdo);
+	$motMan = new MotManager($pdo);
+	$isACorrection = !$motMan->isPhraseOk($_POST["cit"])
 ?>
-<?php if(!empty($_POST["ense"])){
+
+<?php if(!empty($_POST["cit"]) && !$isACorrection){
   $citMan->insertCitation($_POST["ense"], $_POST["date"], $_POST["cit"]);
   echo '<h4>Insertion effectuée</h4>';
 } ?>
 
 <h1>Ajouter une citation</h1>
-<form action="index.php?page=5" id="ajout" method="post">
 
+<form action="index.php?page=5" id="ajout" method="post">
   Enseignant :
   <select name = "ense">
     <?php foreach ($persMan->getAllEnseignant() as $num =>$nom ) {
       echo '<option value ='.$num.'>'.$nom.'</option>';
     } ?>
   </select><br>
-  Date : <input type="date" name="date"  id="date"><br>
+  Date : <input type="date" name="date" id="date" <?php
+	if ($isACorrection){echo 'value='.'"'.$_POST['date'].'"';}
+	?>><br>
   Citation :
-  <textarea rows="4" cols="50" name ="cit">
+  <textarea rows="4" cols="50" name ="cit" <?php
+	if ($isACorrection){echo 'value='.'"'.$motMan->getCorrectedPhrase($_POST['cit']).'"';}
+	?>>
   </textarea>
   <input type="submit" value="Valider"/>
 </form>
