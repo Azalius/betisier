1db8731c01a2a7991943e182f0e14540573e4ad9
tauvy1 <aurelien.tauvy@etu.unilim.fr>
2017-11-27 15:28:18 +0100

    Fix listerCitation pas connecté

--- include/pages/listerCitation.inc.php
+++ include/pages/listerCitation.inc.php
@@ -11,9 +11,10 @@
 		Actuellement
 		<?php echo $citManager->nbCitations() ?>
 		citations sont enregistrées.
-	<tr><th>Nom de l'enseignant</th><th>Libellé</th><th>Date</th><th>Moyenne des notes</th><?php if ($perManager->isEtu($perManager->getPersFromLogin($_SESSION['user'])->getNum())){
+	<tr><th>Nom de l'enseignant</th><th>Libellé</th><th>Date</th><th>Moyenne des notes</th><?php if (!empty($_SESSION['user'])){
+		if ($perManager->isEtu($perManager->getPersFromLogin($_SESSION['user'])->getNum())){
 		echo "<th>Noter</th>";
-	}?></tr>
+	}}?></tr>
 	<?php
 		foreach ($citations as $citation){?>
 			<?php $NumPersonne = $citation->getPerNum();
@@ -27,13 +28,14 @@
 	</td><td><?php echo $citation -> getCitLib();?>
 	</td><td><?php echo getFrenchDate($citation -> getDate());?>
 	</td><td><?php echo $note;?>
-	</td><?php if ($perManager->isEtu($perManager->getPersFromLogin($_SESSION['user'])->getNum()) && $noteManager->getNoteCitationPersonne($NumCitation,$perManager->getPersFromLogin($_SESSION['user'])->getNum())){
+	</td><?php if (!empty($_SESSION['user'])){
+	if ($perManager->isEtu($perManager->getPersFromLogin($_SESSION['user'])->getNum()) && $noteManager->getNoteCitationPersonne($NumCitation,$perManager->getPersFromLogin($_SESSION['user'])->getNum())){
 		echo "<td>
 		 <img src='image/erreur.png' alt='Déja noté'/>
 		 </td>";} else {
 			 echo "<td>
 			 <a href='index.php?page=16&citation=$NumCitation'><img src='image/modifier.png' alt='Déja noté'/></a>
-			 </td>";}
+			 </td>";}}
 		  ?>
 	 </tr>
 	<?php }?>
