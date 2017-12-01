b31331bfc9ee2ff450a830c55759b91b2b6644b0
tauvy1 <aurelien.tauvy@etu.unilim.fr>
2017-12-01 09:44:58 +0100

    Gestion affichage liste sur le coté

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -4,24 +4,36 @@
 		<p><img class = "icone" src="image/personne.png" alt="Personne"/>Personne</p>
 		<ul>
 			<li><a href="index.php?page=2">Lister</a></li>
+			<?php if ($_SESSION['user']!=''){
+				$pdo = new Mypdo();
+				$persMan = new PersonneManager($pdo);
+				?>
 			<li><a href="index.php?page=1">Ajouter</a></li>
+			<?php if ($persMan->getPersFromLogin($_SESSION['user'])->isAdmin()){ ?>
 			<li><a href="index.php?page=4">Supprimer</a></li>
 			<li><a href="index.php?page=3">Modifier</a></li>
+		<?php }} ?>
 		</ul>
 		<p><img class="icone" src="image/citation.gif"  alt="Citation"/>Citations</p>
 		<ul>
-			<li><a href="index.php?page=5">Ajouter</a></li>
 			<li><a href="index.php?page=6">Lister</a></li>
+			<?php if ($_SESSION['user']!=''){ ?>
+			<li><a href="index.php?page=5">Ajouter</a></li>
 			<li><a href="index.php?page=13">Rechercher</a></li>
+			<?php if ($persMan->getPersFromLogin($_SESSION['user'])->isAdmin()){ ?>
 			<li><a href="index.php?page=9">Valider</a></li>
 			<li><a href="index.php?page=10">Supprimer</a></li>
+		<?php }} ?>
 		</ul>
 		<p><img class = "icone" src="image/ville.png" alt="Ville"/>Ville</p>
 		<ul>
 			<li><a href="index.php?page=8">Lister</a></li>
+			<?php if ($_SESSION['user']!=''){ ?>
 			<li><a href="index.php?page=7">Ajouter</a></li>
 			<li><a href="index.php?page=11">Modifier</a></li>
+			<?php if ($persMan->getPersFromLogin($_SESSION['user'])->isAdmin()){ ?>
 			<li><a href="index.php?page=12">Supprimer</a></li>
+		<?php	}} ?>
 		</ul>
 	</div>
 </div>
